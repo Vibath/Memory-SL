710852c7da6103e54435289bbc8f480a384ef88b
Vibath Jayasundara <vibathmkj0827@gmail.com>
2026-09-19 10:43:17 +0530

    Update Google Drive link for wedding packages

--- src/components/WeddingPackages.jsx
+++ src/components/WeddingPackages.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function WeddingPackages() {
   // Replace this with your actual Google Drive PDF link
-  const driveLink = "https://drive.google.com/file/d/19bg_TsEeLRcBaS-PGse3ppOKPtlbAmqV/view?usp=drivesdk";
+  const driveLink = "https://drive.google.com/file/d/186r3Kd-XqLB2oOX0t9GTnSB_ysfftQf0/view?usp=drivesdk";
 
   return (
     <section
